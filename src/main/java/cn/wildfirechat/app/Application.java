1db836603aed1d328892869b00fc7f0f9b85b328
wayaya <dongsibei@ofworld.com>
2020-04-27 14:20:34 +0800

    打印springboot加载的bean

--- src/main/java/cn/wildfirechat/app/Application.java
+++ src/main/java/cn/wildfirechat/app/Application.java
@@ -1,30 +1,44 @@
 package cn.wildfirechat.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        logger.debug("bean总数:{}", ctx.getBeanDefinitionCount());
+
+        int i = 0;
+        for (String name : beanNames) {
+            logger.debug("{} beanName:{}", ++i, name);
+        }
+    }
 
 
-	/**
-	 * 文件上传配置
-	 * @return 配置文件对象
-	 */
-	@Bean
-	public MultipartConfigElement multipartConfigElement() {
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-		//单个文件最大
-		factory.setMaxFileSize("20480KB"); //20MB
-		/// 设置总上传数据总大小
-		factory.setMaxRequestSize("102400KB");
-		return factory.createMultipartConfig();
-	}
+    /**
+     * 文件上传配置
+     *
+     * @return 配置文件对象
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize("20480KB"); //20MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
 }
